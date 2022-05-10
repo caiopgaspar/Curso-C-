69b13deac6d059849756f59b8e8754ace3a38387
caiopgaspar <c-gaspar@hotmail.com>
2022-05-10 12:12:16 -0300

    Dois exercicios Composicao

---
+++ Enume-compos/ExerCompos2/ExerCompos2/Entities/Comment.cs
@@ -0,0 +1,16 @@
+﻿namespace ExerCompos2.Entities
+{
+    internal class Comment
+    {
+        public string Text { get; set; }
+
+        //Constru
+
+        public Comment() { }
+
+        public Comment(string text)
+        {
+            Text = text;
+        }
+    }
+}
---
+++ Enume-compos/ExerEnumComp3/ExerEnumComp3/Entities/Enum/OrderStatus.cs
@@ -0,0 +1,10 @@
+﻿namespace ExerEnumComp3.Entities.Enum
+{
+    enum OrderStatus : int
+    {
+        PendingPayment = 0,
+        Processing = 1,
+        Shipped = 2,
+        Delivered = 3
+    }
+}
